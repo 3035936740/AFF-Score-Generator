2d9e4201d6657b6192b07ec1b971daa8cd6b1799
Bing <3035936740@qq.com>
2025-06-24 22:50:01 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import asyncio
+from generator import GenerateContent, ScoreDetail
+
+async def main():
+    aff_full_path = "songs/vulcanus/2.aff"
+    
+    content : ScoreDetail  = GenerateContent(aff_full_path, is_content=False)
+    print(content)
+    
+    aff_full_path = "songs/alterego/2.aff"
+
+    with open(aff_full_path, 'r', encoding='utf-8') as file:
+        aff_content = file.read()
+    
+    content : ScoreDetail  = GenerateContent(aff_content, is_content=True)
+    print(content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
